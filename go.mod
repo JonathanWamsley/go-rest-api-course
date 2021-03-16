099ca005e2573d618694739580eac47eae3b8e19
jonny <jonnywamsley@github.com>
2021-03-16 09:41:58 -0400

    adding end to end tests for endpoints

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/TutorialEdge/go-rest-api-course v0.0.0-20210206123738-a8401b97883e
+	github.com/go-resty/resty/v2 v2.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.7.0
 )
---
+++ test/comment_test.go
@@ -0,0 +1,64 @@
+// +build e2e
+
+package test
+
+import (
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(`{"slug": "/", "author": "123456", "body": "hello world"}`).
+		Post(BASE_URL + "/api/comment")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestGetAllComments(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().Get(BASE_URL + "/api/comment")
+	if err != nil {
+		t.Fail()
+	}
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestGetComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		Get(BASE_URL + "/api/comment/1")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestPutComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(`{"slug": "/put", "author": "999", "body": "put world"}`).
+		Put(BASE_URL + "/api/comment/1")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestDeleteComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		Delete(BASE_URL + "/api/comment/1")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode())
+}
